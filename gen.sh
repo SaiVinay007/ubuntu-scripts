47600696ba448532d0d489449aa0fa2a88640d64
SaiVinay007 <g.saivinay007.com>
2018-07-11 00:36:04 +0530

    initial scripts

--- gen.sh
+++ gen.sh
@@ -1,9 +1,10 @@
+
 sudo apt-get update
 sudo apt-get install python-pip python-dev build-essential
 sudo apt-get install python3-pip python3-dev  
 sudo pip install --upgrade pip 
 sudo pip3 install --upgrade pip3 
-
+ 
 
 pip install tensorflow
 pip3 install tensorflow
